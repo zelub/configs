5a86f049eaa07efcb9e81e799952e80b0694951f
zelub <100231056+zelub@users.noreply.github.com>
2022-08-04 18:42:40 +0200

    configs

---
+++ dxhd/dxhd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## Launch chromium
+# super + w
+chromium
+
+## Pamixer volume control
+# super + f8
+pamixer -i 3
+# super + shift + f8
+pamixer -i 8
+# super + f7
+pamixer -d 3
+# super + shift + f7
+pamixer -d 8
